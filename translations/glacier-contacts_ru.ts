f8699d6a2c7835416fa5ba64824babf4a3fde91f
Jozef Mlich <jmlich83@gmail.com>
2021-09-19 16:20:11 +0000

    fix ru translation - malformed xml

--- translations/glacier-contacts_ru.ts
+++ translations/glacier-contacts_ru.ts
@@ -106,7 +106,12 @@
         <location filename="../src/qml/components/ContactImportSheet.qml" line="98"/>
         <source>Imported %n contacts</source>
         <comment>ContactImportSheet</comment>
-        <translation><numerusform>Импортировано %n контакт</numerusform><numerusform>Импортировано %n контактов</numerusform><numerusform>Импортировано %n контактов</numerusform><numerusform>Импортировано %n контактов</numerusform></translation>
+        <translation>
+            <numerusform>Импортировано %n контакт</numerusform>
+            <numerusform>Импортировано %n контактов</numerusform>
+            <numerusform>Импортировано %n контактов</numerusform>
+        </translation>
+    </message>
 </context>
 <context>
     <name>ContactListPage</name>
