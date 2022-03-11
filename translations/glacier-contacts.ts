ad4839aa9c1a36005313f314101c6ca335e2362b
Jozef Mlich <jmlich83@gmail.com>
2022-03-11 14:32:15 +0100

    lupdate

--- translations/glacier-contacts.ts
+++ translations/glacier-contacts.ts
@@ -5,20 +5,24 @@
     <name>AvatarPickerSheet</name>
     <message>
         <location filename="../src/qml/components/AvatarPickerSheet.qml" line="49"/>
+        <location filename="../src/qml/components/AvatarPickerSheet.qml" line="49"/>
         <source>Add contact photo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/AvatarPickerSheet.qml" line="91"/>
         <location filename="../src/qml/components/AvatarPickerSheet.qml" line="91"/>
         <source>No photo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/AvatarPickerSheet.qml" line="106"/>
+        <location filename="../src/qml/components/AvatarPickerSheet.qml" line="106"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/AvatarPickerSheet.qml" line="119"/>
         <location filename="../src/qml/components/AvatarPickerSheet.qml" line="119"/>
         <source>Select</source>
         <translation type="unfinished"></translation>
@@ -27,17 +31,20 @@
 <context>
     <name>ContactCardContentWidget</name>
     <message>
-        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="103"/>
+        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="114"/>
+        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="114"/>
         <source>Call</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="116"/>
+        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="127"/>
+        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="127"/>
         <source>SMS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="131"/>
+        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="142"/>
+        <location filename="../src/qml/components/ContactCardContentWidget.qml" line="142"/>
         <source>Mail</source>
         <translation type="unfinished"></translation>
     </message>
@@ -46,41 +53,49 @@
     <name>ContactEditPage</name>
     <message>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="51"/>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="51"/>
         <source>Add contact</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="51"/>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="51"/>
         <source>Edit contact</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="130"/>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="130"/>
         <source>First name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="144"/>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="144"/>
         <source>Last name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="170"/>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="170"/>
         <source>Phones</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="188"/>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="188"/>
         <source>Email address</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/ContactEditPage.qml" line="196"/>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="196"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/pages/ContactEditPage.qml" line="210"/>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="211"/>
+        <location filename="../src/qml/pages/ContactEditPage.qml" line="211"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
@@ -89,35 +104,42 @@
     <name>ContactImportSheet</name>
     <message>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="121"/>
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="121"/>
         <source>Import</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="106"/>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="106"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="50"/>
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="50"/>
         <source>Import contacts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="99"/>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="99"/>
         <source>Please copy *.vcf into %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="147"/>
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="147"/>
         <source>Import completed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="148"/>
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="148"/>
         <source>Ok</source>
         <translation type="unfinished"></translation>
     </message>
     <message numerus="yes">
+        <location filename="../src/qml/components/ContactImportSheet.qml" line="146"/>
         <location filename="../src/qml/components/ContactImportSheet.qml" line="146"/>
         <source>Imported %n contacts</source>
         <comment>ContactImportSheet</comment>
@@ -131,30 +153,36 @@
     <name>ContactListPage</name>
     <message>
         <location filename="../src/qml/pages/ContactListPage.qml" line="51"/>
+        <location filename="../src/qml/pages/ContactListPage.qml" line="51"/>
         <source>Contacts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/ContactListPage.qml" line="64"/>
         <location filename="../src/qml/pages/ContactListPage.qml" line="64"/>
         <source>Import contacts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/ContactListPage.qml" line="74"/>
+        <location filename="../src/qml/pages/ContactListPage.qml" line="74"/>
         <source>Search</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/ContactListPage.qml" line="138"/>
         <location filename="../src/qml/pages/ContactListPage.qml" line="138"/>
         <source>Ok</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/ContactListPage.qml" line="139"/>
+        <location filename="../src/qml/pages/ContactListPage.qml" line="139"/>
         <source>Export completed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/ContactListPage.qml" line="140"/>
         <location filename="../src/qml/pages/ContactListPage.qml" line="140"/>
         <source>Export completed to </source>
         <translation type="unfinished"></translation>
@@ -177,20 +205,24 @@
     <name>DeleteContactDialog</name>
     <message>
         <location filename="../src/qml/components/DeleteContactDialog.qml" line="49"/>
+        <location filename="../src/qml/components/DeleteContactDialog.qml" line="49"/>
         <source>Delete contact</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/DeleteContactDialog.qml" line="50"/>
         <location filename="../src/qml/components/DeleteContactDialog.qml" line="50"/>
         <source>Are you sure?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/components/DeleteContactDialog.qml" line="51"/>
+        <location filename="../src/qml/components/DeleteContactDialog.qml" line="51"/>
         <source>Yes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/components/DeleteContactDialog.qml" line="52"/>
         <location filename="../src/qml/components/DeleteContactDialog.qml" line="52"/>
         <source>No</source>
         <translation type="unfinished"></translation>
